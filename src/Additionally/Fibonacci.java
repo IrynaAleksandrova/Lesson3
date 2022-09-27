1e5f6fa0de28ded305b5c3cabe97438cd45e2021
Iryna Lytkina <lytkina.iryna@gmail.com>
2022-09-27 11:38:07 +0300

    third

---
+++ src/Additionally/Fibonacci.java
@@ -0,0 +1,7 @@
+package Additionally;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Cycles/Subsequence2.java
+++ src/Cycles/Subsequence2.java
@@ -3,9 +3,12 @@ package Cycles;
 
 public class Subsequence2 {
     public static void main(String[] args) {
+      /*  for (int i = 0; i > -50; i -= 5) {
+            if (i % 5 == 0) { System.out.print(i); }
+            }  */
         for (int i = 0; i < 50; i++) {
             if (i % 5 == 0) {
-                System.out.print(i*(-1) + " ");
+                System.out.println(i * -1);
             }
         }
     }
