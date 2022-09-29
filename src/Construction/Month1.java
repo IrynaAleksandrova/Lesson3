1c73a871564e5987031b87c4e95c931cbc10f645
Iryna Lytkina <lytkina.iryna@gmail.com>
2022-09-29 19:49:53 +0300

    seventh

--- src/Construction/Month1.java
+++ src/Construction/Month1.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Month1 {
 
     public static void main(String[] args) {
+        System.out.println("Введите номер месяца: ");
         Scanner console = new Scanner(System.in);
         int Month = console.nextInt();
         switch (Month) {
@@ -12,6 +13,7 @@ public class Month1 {
             case 3, 4, 5 -> System.out.println("Весна");
             case 6, 7, 8 -> System.out.println("Лето");
             case 9, 10, 11 -> System.out.println("Осень");
+            default -> System.out.println("Неверно введен номер месяца");
         }
         console.close();
     }
